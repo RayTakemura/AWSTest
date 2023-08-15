aac384add930ce05cc0ff731ebf6b990ede38e55
RayTakemura <raytakemura@gmail.com>
2023-08-14 22:11:52 -0700

    server installed

---
+++ server/index.js
@@ -0,0 +1,27 @@
+const { ApolloServer } = require("@apollo/server");
+const { startStandaloneServer } = require("@apollo/server/standalone");
+const { resolvers } = require("./resolvers.js");
+const { typeDefs } = require("./models/typeDefs.js");
+const { default: mongoose } = require("mongoose");
+
+const MONGO_URI = "mongodb://localhost:27017/student-register";
+
+mongoose
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log(`Db Connected`);
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+startStandaloneServer(server, {
+  listen: { port: 4000 },
+}).then(({ url }) => {
+  console.log(`Server ready at ${url}`);
+});
